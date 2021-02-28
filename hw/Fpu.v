1a83a969753a20a7b4695f312ea242a9cfe67212
PENGUINLIONG <admin@penguinliong.moe>
2021-02-28 20:20:24 +0800

    FPU

--- hw/Fpu.v
+++ hw/Fpu.v
@@ -3,42 +3,46 @@
 // Unit. This is expected to achieve better precision then floating point
 // arithmetics while retaining better performance.
 
-// FPU opcode table:
-// ____________________________________
-// |         |                        |
-// | ALU OP  | Description            |
-// |---------|------------------------|
-// | 4'b0000 | addition               |
-// | 4'b0001 | subtranction           |
-// | 4'b0010 | multiplication         |
-// | 4'b0011 | division               |
-// |---------|------------------------|
-// | 4'b0100 | maximum                |
-// | 4'b0101 | minimum                |
-// |---------|------------------------|
-// | 4'b1011 | xor                    |
-// |---------|------------------------|
-// | 4'b1101 | set-less-than signed   |
-// |_________|________________________|
-//
 module Fpu (
-  input [3:0] alu_op,
+  input clk,
+  input mul_div,
+  input neg_a,
+  input neg_c,
   input signed [63:0] a_data,
   input signed [63:0] b_data,
   input signed [63:0] c_data,
 
-  output [63:0] alu_res,
-  output zero
+  output busy,
+  output signed [63:0] fpu_res
 );
 
-  assign alu_res =
-    alu_op == 4'b000 ? a_data + b_data :
-    alu_op == 4'b001 ? a_data - b_data :
-    alu_op == 4'b010 ? a_data * b_data :
-    alu_op == 4'b011 ? a_data / b_data :
-    alu_op == 4'b100 ? 
-    alu_op == 4'b101 ?
-    alu_op == 4'b110 ?
-    alu_op == 4'b111 ?
+  wire signed [63:0] a = neg_a ? -a_data : a_data;
+  wire signed [63:0] b = b_data;
+  wire signed [63:0] c = neg_c ? -c_data : c_data;
+
+  assign busy = div_busy;
+
+  wire signed [63:0] mad_res;
+  Q15Madder mad(
+    .a(a),
+    .b(b),
+    .c(c),
+    .res(mad_res)
+  );
+
+  wire launch_div = mul_div & !div_busy;
+  wire div_busy;
+  wire signed [63:0] div_res;
+  Q15Divider div(
+    .clk(clk),
+    .reset(reset),
+    .launch(launch_div),
+    .a(a),
+    .b(b),
+    .busy(div_busy),
+    .res(div_res)
+  );
+
+  assign fpu_res = mul_div ? div_res : mad_res;
 
 endmodule
