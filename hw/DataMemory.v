625139b9ca8b333b2864f7c70a1cae3ea40c89a3
PENGUINLIONG <admin@penguinliong.moe>
2021-03-05 14:44:40 +0800

    Allow variable data memory size

--- hw/DataMemory.v
+++ hw/DataMemory.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ps
 
-module DataMemory(
+module DataMemory #(parameter NWORD=8192) (
   input clk,
   input reset,
 
@@ -13,10 +13,10 @@ module DataMemory(
   output [31:0] read_data
 );
 
-  reg [31:0] inner [1023:0];
+  reg [31:0] inner [NWORD - 1:0];
 
-  wire word_aligned_instr_addr = { 2'b00, instr_addr[31:2] };
-  wire word_aligned_data_addr  = { 2'b00,  data_addr[31:2] };
+  wire [31:0] word_aligned_instr_addr = { 2'b00, instr_addr[31:2] };
+  wire [31:0] word_aligned_data_addr  = { 2'b00,  data_addr[31:2] };
 
   assign instr = inner[word_aligned_instr_addr];
   assign read_data = inner[word_aligned_data_addr];
@@ -24,7 +24,7 @@ module DataMemory(
   integer i;
   always @(posedge clk, posedge reset) begin
     if (reset) begin
-      for (i = 0; i < 1024; i = i + 1) begin
+      for (i = 0; i < NWORD; i = i + 1) begin
         inner[i] = 0;
       end
     end
