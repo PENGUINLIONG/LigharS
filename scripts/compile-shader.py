b7e290b4b8fea1127e434ffa9cfb8243bee6d4dd
PENGUINLIONG <admin@penguinliong.moe>
2021-03-06 04:39:55 +0800

    Support large static and global variable data sections

--- scripts/compile-shader.py
+++ scripts/compile-shader.py
@@ -126,12 +126,18 @@ def extract_section_table():
 def extract_cmdbuf_content(section_offset_map, section_size_map, obj):
     text_offset  = section_offset_map[".text"]  if ".text"  in section_offset_map else 0
     sdata_offset = section_offset_map[".sdata"] if ".sdata" in section_offset_map else 0
+    sbss_offset  = section_offset_map[".sbss"]  if ".sbss"  in section_offset_map else 0
+    data_offset  = section_offset_map[".data"]  if ".data"  in section_offset_map else 0
     text_size  = section_size_map[".text"]  if ".text"  in section_size_map else 0
     sdata_size = section_size_map[".sdata"] if ".sdata" in section_size_map else 0
+    sbss_size  = section_size_map[".sbss"]  if ".sbss"  in section_size_map else 0
+    data_size  = section_size_map[".data"]  if ".data"  in section_size_map else 0
     assert text_size != 0, "no instruction to run in this shader"
 
-    text_content  = obj[text_offset:(text_offset + text_size)]
+    text_content  = obj[text_offset :(text_offset  + text_size )]
     sdata_content = obj[sdata_offset:(sdata_offset + sdata_size)]
+    sbss_content  = obj[sbss_offset :(sbss_offset  + sbss_size )]
+    data_content  = obj[data_offset :(data_offset  + data_size )]
     content = text_content + sdata_content
     assert len(content) % 4 == 0, f"command buffer size must align to 4, but is {len(content)}"
 
@@ -185,19 +191,27 @@ def extract_symbol_table(section_size_map):
 def map_symbol_offsets(section_size_map, symbol_map):
     text_size  = section_size_map[".text"]  if ".text"  in section_size_map else 0
     sdata_size = section_size_map[".sdata"] if ".sdata" in section_size_map else 0
+    sbss_size  = section_size_map[".sbss"]  if ".sbss"  in section_size_map else 0
+    data_size  = section_size_map[".data"]  if ".data"  in section_size_map else 0
 
     assert text_size != 0, "no instruction to run in this shader"
 
     text_offset  = 0
-    sdata_offset = text_size
+    sdata_offset = text_offset  + text_size
+    sbss_offset  = sdata_offset + sdata_size
+    data_offset  = sbss_offset + sbss_size
     symbol_offset_map = {}
     for symbol, (section, offset) in symbol_map.items():
         if section == "*ABS*":
             symbol_offset_map[symbol] = offset
         elif section == ".text":
-            symbol_offset_map[symbol] = text_offset + offset
+            symbol_offset_map[symbol] = text_offset  + offset
         elif section == ".sdata":
             symbol_offset_map[symbol] = sdata_offset + offset
+        elif section == ".sbss":
+            symbol_offset_map[symbol] = sbss_offset  + offset
+        elif section == ".data":
+            symbol_offset_map[symbol] = data_offset  + offset
         else:
             assert False, f"unsupported data section {section}"
 
@@ -247,6 +261,14 @@ def relocate_symbols(symbol_offset_map, words):
         elif opcode == 0x19:
             # jalr
             word += imm12 << 20
+        elif opcode == 0x18:
+            # beq, or other branch ops
+            imm12_align2 = (symbol_offset >> 1) & 0xfff
+            a_imm12_align2 = imm12_align2 >> 11
+            b_imm12_align2 = (imm12 >> 10) & 1
+            c_imm12_align2 = (imm12 >> 4) & 0b111111
+            d_imm12_align2 = imm12 & 0xf
+            word += (a_imm12_align2 << 31) | (b_imm12_align2 << 7) | (c_imm12_align2 << 8) | (d_imm12_align2 << 25)
         elif opcode == 0x00 or opcode == 0x01:
             # lw, flw
             word += imm12 << 20
